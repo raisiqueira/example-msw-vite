f6b3b3cca40ae1b897bc96d25d93301b66b1e6da
Raí Siqueira <me@raisiqueira.io>
2022-10-21 22:50:58 +0000

    test: setup MSW in vitest config

--- src/setup-test.ts
+++ src/setup-test.ts
@@ -1,1 +1,18 @@
 import '@testing-library/jest-dom'
+import { afterAll, afterEach, beforeAll } from 'vitest'
+
+import { server } from './mocks'
+
+beforeAll(() => {
+  server.listen()
+})
+
+beforeAll(() => {
+  server.resetHandlers()
+})
+
+afterEach(() => server.resetHandlers())
+
+afterAll(() => {
+  server.close()
+})
